620c019bc8c37355d1f605bd9b78039e07daa5c1
sathviknprasad <sathviknp@gmail.com>
2014-08-12 13:54:59 +0530

    Made the example work with my own input

--- test/test.py
+++ test/test.py
@@ -3,16 +3,19 @@ import sys
 import hashlib
 lib_path = os.path.abspath('..')
 sys.path.append(lib_path)
+sys.path.append('~/cloud_convert')
+
 
 import CloudConvert
 
 apikey = open("apikey.txt", "r").read().strip()
 
+print apikey
 process = CloudConvert.ConversionProcess(apikey)
 
-print("possible?", CloudConvert.CloudConvert.is_possible("mp3", "ogg"))
+print("possible?", CloudConvert.CloudConvert.is_possible("jpg", "png"))
 
-process.init("pink.mp3", "out.ogg")
+process.init("i1.jpg", "o1.png")
 
 print("from", process.fromformat)
 print("to", process.toformat)
